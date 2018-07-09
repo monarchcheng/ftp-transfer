fe67cc16a0f04972fc086078a3e7d432828ba280
monarchcheng <monarchcheng1993@gmail.com>
2018-07-09 11:06:04 +0800

    modified:   src/main/java/ftp/controller/FtpTransferController.java

--- src/main/java/ftp/controller/FtpTransferController.java
+++ src/main/java/ftp/controller/FtpTransferController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,13 +15,33 @@ import java.util.List;
 @RestController
 public class FtpTransferController {
 
-    @RequestMapping("/getpic")
-    public void ftpTransfer(@RequestParam String url, HttpServletResponse response){
+    /**
+     * 转发获取图片（直接展示）
+     * @param url  文件路径
+     * @param response
+     */
+    @RequestMapping("/img")
+    public void ftpTransferImg(@RequestParam String url, HttpServletResponse response){
         try {
-//            List<String> list = Arrays.asList(url.split("/"));  //转发文件时可设置文件名
-//            String fileName = list.get(list.size()-1) ;
-//            response.setCharacterEncoding("UTF-8");
-//            response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+            FTPUtils.transfer(response.getOutputStream(),url);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 转发文件（直接下载）
+     * @param url  文件路径
+     * @param response
+     */
+    @RequestMapping("/file")
+    public void ftpTransferFile(@RequestParam String url, HttpServletResponse response){
+        try {
+            List<String> list = Arrays.asList(url.split("/"));
+            String fileName = list.get(list.size()-1) ;
+            response.setCharacterEncoding("UTF-8");
+            response.addHeader("Content-Disposition", "attachment; filename=" +URLEncoder.encode(fileName,"UTF-8") );
             FTPUtils.transfer(response.getOutputStream(),url);
         }catch (IOException e){
             e.printStackTrace();
